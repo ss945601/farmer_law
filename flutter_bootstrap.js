0c643787c874388fcd4b6a8c935569f08ff33fce
Steven Chang <steven.chang@latticeworkinc.com>
2026-09-02 17:17:59 +0800

    fix ai

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d3d45dcf251823c1769909cd43698d126db38d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2082881833"
+    serviceWorkerVersion: "236359335"
   }
 });
